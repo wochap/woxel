4dc5680aaa034e30d416948b3cc57421a1cb5103
wochap <gean.marroquin@gmail.com>
2025-03-29 11:47:17 -0500

    style: formatting

--- main.cpp
+++ main.cpp
@@ -1,13 +1,12 @@
 #include <QApplication>
 #include <QLabel>
 
-int main(int argc, char *argv[])
-{
-    QApplication app(argc, argv);
-    
-    QLabel label("Hello World!");
-    label.resize(200, 100);
-    label.show();
-    
-    return app.exec();
+int main(int argc, char *argv[]) {
+  QApplication app(argc, argv);
+
+  QLabel label("Hello World!");
+  label.resize(200, 100);
+  label.show();
+
+  return app.exec();
 }
